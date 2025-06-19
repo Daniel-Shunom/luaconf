6b823d7e66e7f5c28c00c1472cf09de84abcd5c6
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-06-19 19:50:46 -0400

    enable pyright

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,8 @@
 require("nvchad.configs.lspconfig").defaults()
+require('lspconfig').pyright.setup({})
 
 local servers = { "html", "cssls" }
 vim.lsp.enable(servers)
+vim.lsp.enable('pyright')
 
 -- read :h vim.lsp.config for changing options of lsp servers 
