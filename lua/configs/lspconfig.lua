eae75cfb9812f2f6648d1321d4ee5b658ecd3fb8
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-06-19 21:09:32 -0400

    addes lsps

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,8 +1,55 @@
 require("nvchad.configs.lspconfig").defaults()
-require('lspconfig').pyright.setup({})
 
 local servers = { "html", "cssls" }
 vim.lsp.enable(servers)
-vim.lsp.enable('pyright')
 
--- read :h vim.lsp.config for changing options of lsp servers 
+vim.lsp.enable('pyright')
+vim.lsp.enable('gleam')
+vim.lsp.enable('erlang')
+vim.lsp.enable('cmake')
+vim.lsp.enable('tailwind')
+
+vim.lsp.config('ts_ls', {
+  init_options = { hostInfo = 'neovim' },
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = {
+    'javascript',
+    'javascriptreact',
+    'javascript.jsx',
+    'typescript',
+    'typescriptreact',
+    'typescript.tsx',
+  },
+  root_markers = { 'tsconfig.json', 'jsconfig.json', 'package.json', '.git' },
+  handlers = {
+    ['_typescript.rename'] = function(_, result, ctx)
+      local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+      vim.lsp.util.show_document({
+        uri = result.textDocument.uri,
+        range = {
+          start = result.position,
+          ['end'] = result.position,
+        },
+      }, client.offset_encoding)
+      vim.lsp.buf.rename()
+      return vim.NIL
+    end,
+  },
+  on_attach = function(client)
+    vim.api.nvim_buf_create_user_command(0, 'LspTypescriptSourceAction', function()
+      local source_actions = vim.tbl_filter(function(action)
+        return vim.startswith(action, 'source.')
+      end, client.server_capabilities.codeActionProvider.codeActionKinds)
+
+      vim.lsp.buf.code_action({
+        context = {
+          only = source_actions,
+        },
+      })
+    end, {})
+  end,
+})
+
+vim.lsp.enable('ts_ls')
+
+-- read :h vim.lsp.config for changing options of lsp servers
