8d8189c9bdda585fea3997fa5557e58872b26b1c
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-06-24 22:54:20 -0400

    custom configs

--- lua/options.lua
+++ lua/options.lua
@@ -54,5 +54,13 @@ vim.cmd("syntax on")                  -- Enable syntax highlighting
 
 vim.opt.list = true
 vim.opt.listchars = { tab = "→ ", trail = "·", nbsp = "␣" }
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function() 
+    vim.highlight.on_yank()
+  end,
+})
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
