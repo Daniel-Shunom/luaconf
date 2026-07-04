bff97fa202bcc8d41dbf32ea2250e794bf77cb4d
Daniel S Jeremiah <danielshunom2@gmail.com>
2026-07-04 14:19:21 -0400

    support for bashls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "bashls" }
 vim.lsp.enable(servers)
 
 vim.lsp.enable('pyright')
